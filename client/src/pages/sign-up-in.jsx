a172daf6fefd6c8c970bb9619e6acd177becd1a3
bou1der <krowkin@bk.ru>
2024-01-24 15:06:42 +0300

    C:/Programms/Git/

--- client/src/pages/sign-up-in.jsx
+++ client/src/pages/sign-up-in.jsx
@@ -23,6 +23,7 @@ function SignPage(){
             {
                 console.log(err);
             })
+
     }
     return(
         <div style={{width: "100%", height:"100%",display:"flex", alignItems: "center", justifyContent: "center"}}>
