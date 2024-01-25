2a032fa8ef02f889910c5260cfb3202f3cab5f46
bou1der <krowkin@bk.ru>
2024-01-25 17:37:40 +0300

    gen token

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const auht = require('./routers/auth');
-const User = require('./models/user')
-class UserClass {
-    id;
-    login;
-    isActivated;
-    constructor(model) {
-        this.id = model.id;
-        this.login = model.login;
-        this.isActivated = model.isActivated;
-    }
-}
-const existUser = User.findOne({where:{login:'boulder'},})
-console.log(existUser)
-const newUser = new UserClass(existUser)
-console.log(newUser)
-
-
+const cookieParser = require('cookie-parser')
 const app = express();
 
 app.use(require('cors')())
+app.use(cookieParser())
 
 app.use('/api/auth', auht)
 
