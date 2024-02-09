c84a48a0bb5ba276a441f1f9fa2570ed860d1b96
bou1der <krowkin@bk.ru>
2024-02-09 13:26:06 +0300

    axios interceptor/authServiceClient

---
+++ client/src/services/authService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import api from "../http/serviceAxios.js";
+
+
+
+const login = async (loginValue) =>{
+
+    const [loginLogining,passwordLogining] = loginValue
+
+    api.post('/api/auth/login',{
+        login:loginLogining,
+        password:passwordLogining
+    })
+
+    await axios.post("http://localhost:8000/api/auth/login",{
+        login:loginLogining,
+        password:passwordLogining
+    }).then((res) => {
+        if (res.status === 200){
+            location.reload()
+        }
+        console.log(res);
+    }).catch((err)=> {console.log(err)})
+}
+
+
+export {login}
